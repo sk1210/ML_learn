05117eda149aee3a818876a6c57d568b78870da9
Shahrukh khan <khanskid111@gmail.com>
2018-05-22 10:29:36 +0530

    Update loadData.py

--- models/loadData.py
+++ models/loadData.py
@@ -2,44 +2,53 @@ import os
 import pickle
 import tensorflow as tf
 
-fileNames = "./../data/labels.pkl"
-f= open(fileNames,"rb")
-data = pickle.load(f)
-f.close()
-
-
+imgPath = "./../data/train/"
+fileNames = os.listdir("./../data/train")
+#fileNames = [ imgPath + img for img in fileNames]
 total_classes = 228
 #one hot encod all data
 
-
-
-def multiple_one_hot(cat_tensor, depth_list):
-
-    one_hot_enc_tensor = tf.one_hot(cat_int_tensor[:,0], depth_list[0], axis=1)
-    for col in range(1, len(depth_list)):
-        add = tf.one_hot(cat_int_tensor[:,col], depth_list[col], axis=1)
-        one_hot_enc_tensor = tf.concat([one_hot_enc_tensor, add], axis=1)
-
-    return one_hot_enc_tensor
-
-def splitImgLabels(imageName):
-    print (imageName)
-    imgId = str(imageName)+".jpg"
-    #labels = data[imageName]
-    return (imgId),[0]*10
-
-
-
+f = open("./../data/labels.pkl","rb")
+labels = pickle.load(f)
+f.close()
 ######################
 
 sess = tf.Session()
+#sess = tf.InteractiveSession()
 
-train_data = {}
-for d in data:
-    one_hot = tf.one_hot(indices = data[d],depth= total_classes)
-    train_data[d] = tf.reduce_sum(one_hot, reduction_indices=0)
+def my_func(input):
+    print (input)
+    return tf.Variable([0,1,1,0],dtype=tf.int8)
+def getLabels(image):
+    global labels
 
-ds = tf.data.Dataset.from_tensor_slices(train_data)
-ds.map(splitImgLabels)
+    #tf.Print ("HELLO",data=[image])
 
-print ((data.keys))
+    image_string = tf.read_file(imgPath+image)
+    image_decoded =  tf.image.decode_jpeg(image_string)
+    image_resized = tf.image.resize_images(image_decoded, [28, 28])
+    #img = tf.decode_j
+    # label  = tf.py_func(my_func, [image], tf.string ,Tout = [tf.int8,])
+
+    print ("ID:",sess.run(image))
+    #one_hot = tf.one_hot(indices=labels, depth=total_classes)
+    #train_data[d] = tf.reduce_sum(one_hot, reduction_indices=0)
+    #return (1, 1)
+    return(image_resized,1)
+
+temp= [0]*len(fileNames)
+ds = tf.data.Dataset.from_tensor_slices((fileNames))
+#ds.map(getLabels)
+ds = ds.map(getLabels)
+ds = ds.batch(1)
+
+iterator = ds.make_initializable_iterator()
+
+
+init = tf.global_variables_initializer()
+sess.run(init)
+# Initialize `iterator` with training data.
+#training_filenames = images
+#print (sess.run(iterator.initializer))
+#print(sess.run(iterator.get_next()))
+
