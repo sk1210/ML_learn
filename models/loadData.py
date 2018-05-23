ae12ce628421e379944e93bd34225cb3693d1fd4
Shahrukh khan <khanskid111@gmail.com>
2018-05-23 08:46:05 +0530

    Update loadData.py

--- models/loadData.py
+++ models/loadData.py
@@ -4,13 +4,23 @@ import tensorflow as tf
 
 imgPath = "./../data/train/"
 fileNames = os.listdir("./../data/train")
-#fileNames = [ imgPath + img for img in fileNames]
+fileNames = [ img for img in fileNames]
 total_classes = 228
 #one hot encod all data
 
 f = open("./../data/labels.pkl","rb")
 labels = pickle.load(f)
 f.close()
+multilabels = []
+images = []
+for i,img in enumerate(fileNames):
+    if i>10000:break
+    id  = int(img.split(".")[0])
+    if not id in labels:continue
+
+    one_hot = tf.one_hot(indices=labels[id], depth=total_classes)
+    multilabels.append(tf.reduce_sum(one_hot, reduction_indices=0))
+    images.append(img)
 ######################
 
 sess = tf.Session()
@@ -18,8 +28,9 @@ sess = tf.Session()
 
 def my_func(input):
     print (input)
-    return tf.Variable([0,1,1,0],dtype=tf.int8)
-def getLabels(image):
+    one_hot = tf.one_hot(1, depth=total_classes)
+    return [2,2]
+def getLabels(image,label):
     global labels
 
     #tf.Print ("HELLO",data=[image])
@@ -28,19 +39,23 @@ def getLabels(image):
     image_decoded =  tf.image.decode_jpeg(image_string)
     image_resized = tf.image.resize_images(image_decoded, [28, 28])
     #img = tf.decode_j
-    # label  = tf.py_func(my_func, [image], tf.string ,Tout = [tf.int8,])
+    #label  = tf.py_func(my_func, [image], tf.string ,Tout = tf.int8)
 
-    print ("ID:",sess.run(image))
+    #print ("ID:",sess.run(image))
+
+
+    #enc = labels[image]
     #one_hot = tf.one_hot(indices=labels, depth=total_classes)
     #train_data[d] = tf.reduce_sum(one_hot, reduction_indices=0)
     #return (1, 1)
-    return(image_resized,1)
+    return(image_resized,label,image)
 
 temp= [0]*len(fileNames)
-ds = tf.data.Dataset.from_tensor_slices((fileNames))
+
+ds = tf.data.Dataset.from_tensor_slices((images,multilabels))
 #ds.map(getLabels)
 ds = ds.map(getLabels)
-ds = ds.batch(1)
+ds = ds.batch(32)
 
 iterator = ds.make_initializable_iterator()
 
@@ -49,6 +64,5 @@ init = tf.global_variables_initializer()
 sess.run(init)
 # Initialize `iterator` with training data.
 #training_filenames = images
-#print (sess.run(iterator.initializer))
-#print(sess.run(iterator.get_next()))
-
+sess.run(iterator.initializer)
+print(sess.run(iterator.get_next()))
