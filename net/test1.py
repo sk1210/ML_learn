28d6b7171c30fc278b469d7f00113ec5ea103ec8
Shahrukh khan <khanskid111@gmail.com>
2018-10-11 17:53:04 +0530

    Create test1.py

---
+++ net/test1.py
@@ -0,0 +1,109 @@
+from sklearn import datasets
+import numpy as np
+from matplotlib import pyplot
+from sklearn.preprocessing import OneHotEncoder
+
+class Network:
+    def __init__(self):
+        self.layers = []
+        self.input = None
+
+        self.output = None
+
+        self.x = None
+        self.y = None
+
+        self.y_hot = None
+        self.classes = None
+
+        self.delta_ouput = None
+
+    def forward(self):
+        input = self.input
+
+        for i,l in enumerate(self.layers):
+            print ("Layer " , i)
+            input = l.forward_prop(input)
+            print (input.shape , input)
+        net.output = input
+
+    def backward(self):
+        for i,l in enumerate(self.layers):
+            
+
+    def calculate_loss(self,type):
+        net.y_hot = self.one_hot(self.classes,y)
+
+        if type == "sse":
+            loss = (net.output - net.y_hot) ** 2
+            avg_loss = np.sum(loss)/len(y)
+
+        self.delta_ouput = loss
+        return loss,avg_loss
+
+    def one_hot(self, n, x):
+        encod = []
+        for i in x:
+            temp = [0] * n
+            temp[i] = 1
+            encod.append(temp)
+        return np.array(encod)
+
+class Layer:
+
+    def __init__(self, input_shape, output_shape, activation ):
+        self.weights =  np.random.rand(input_shape,output_shape)
+        self.bias = np.random.rand(output_shape)
+
+        self.dWeight = np.random.rand(output_shape,input_shape)
+        self.dBias = None
+
+        self.activation = activation
+        self.shape = output_shape
+
+        self.delta = None
+
+    def forward_prop(self, x ):
+
+        self.output = np.dot( x , self.weights  )
+
+        if self.activation == "relu":
+            self.output = np.max(0,self.output)
+
+        elif self.activation == "sigmoid":
+            self.output = 1/(1+np.exp(self.output))
+
+        return self.output
+
+    def backward_prop(self,x):
+
+        if self.activation =="sigmoid":
+            return x * (1 - x)
+
+    def update_weight(self):
+        pass
+
+
+data = datasets.load_digits()
+x = data.data[:2]
+y = data.target[:2]
+
+input = x.T
+
+layer1 = Layer(x.shape[1],5,"sigmoid")
+layer2 = Layer(layer1.shape,7,"sigmoid")
+layer3 = Layer(layer2.shape,9,"sigmoid")
+layer4 = Layer(layer3.shape,10,"sigmoid")
+
+
+net = Network()
+net.classes = 10
+net.layers += [layer1,layer2,layer3,layer4]
+net.input = x
+net.y = y
+
+
+# forward prop
+net.forward()
+loss=  net.calculate_loss("sse")
+print("My Net")
