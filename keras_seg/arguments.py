40d7b1ca3870c69a60275a45ae6bbd4a3ec4b693
Shahrukh khan <khanskid111@gmail.com>
2018-11-25 19:05:59 +0530

    Update arguments.py

--- keras_seg/arguments.py
+++ keras_seg/arguments.py
@@ -12,7 +12,7 @@ n_classes = 2
 input_height = 640
 input_width = 480
 
-save_weights_path = "weights/vgg_segnet"
+save_weights_path = "gdrive/My Drive/My_Projects/weights/vgg_segnet"
 epochs = 50
 load_weights = ""
 optimizer_name = "adadelta"
