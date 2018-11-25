c427c943863af1754db7cc6b458dfcb392117ace
Shahrukh khan <khanskid111@gmail.com>
2018-11-25 20:30:14 +0530

    Update VGGSegnet.py

--- keras_seg/Models/VGGSegnet.py
+++ keras_seg/Models/VGGSegnet.py
@@ -53,7 +53,7 @@ def VGGSegnet( n_classes ,  input_height=416, input_width=608 , vgg_level=3):
 	x = Dense( 1000 , activation='softmax', name='predictions')(x)
 
 	vgg  = Model(  img_input , x  )
-	vgg.load_weights(VGG_Weights_path)
+	#vgg.load_weights(VGG_Weights_path)
 
 	levels = [f1 , f2 , f3 , f4 , f5 ]
 
