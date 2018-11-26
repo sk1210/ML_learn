6ff4d6305a2869ef627ba9036784e70707f09619
Shahrukh khan <khanskid111@gmail.com>
2018-11-27 00:35:12 +0530

    Update arguments.py

--- keras_seg/arguments.py
+++ keras_seg/arguments.py
@@ -4,13 +4,15 @@ label_dir = r"gdrive/My Drive/My_Projects/data/human_seg/ann/"
 img_dir = r"gdrive/My Drive/My_Projects/data/human_seg/img/"
 mask = r"gdrive/My Drive/My_Projects/data/human_seg/mask/"
 
+# for multiple data
+img_dir = r"gdrive/My Drive/My_Projects/data/All_converted/"
 
 train_images_path = img_dir
 train_segs_path = mask
 train_batch_size = 4
 n_classes = 2
-input_height = 640
-input_width = 480
+input_height = 448
+input_width = 448
 
 save_weights_path = "gdrive/My Drive/My_Projects/weights/vgg_segnet"
 epochs = 50
