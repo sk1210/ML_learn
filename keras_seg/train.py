b932fd7bfb49fb9ae58c78bfd0479ac6dd7c1f3b
Shahrukh khan <khanskid111@gmail.com>
2018-11-30 22:21:54 +0530

    Update train.py

--- keras_seg/train.py
+++ keras_seg/train.py
@@ -13,8 +13,8 @@ def train():
 		  metrics=['accuracy'])
 	m.summary()
 
-	load_weights =  "gdrive/My Drive/My_Projects/weights/vgg_unet_448_.148"
-	epoch = 148
+	load_weights =  "gdrive/My Drive/My_Projects/weights/vgg_unet_448_.162"
+	epoch = 162
 	if len( load_weights ) > 0:
 		m.load_weights(load_weights, by_name=False,skip_mismatch=False)
 
