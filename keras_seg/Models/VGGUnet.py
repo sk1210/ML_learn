c18f0a31a364ae2d9d0f693f94b87c5849c0bdee
Shahrukh khan <khanskid111@gmail.com>
2018-12-04 01:45:56 +0530

    Update VGGUnet.py

--- keras_seg/Models/VGGUnet.py
+++ keras_seg/Models/VGGUnet.py
@@ -56,7 +56,7 @@ def VGGUnet( n_classes ,  input_height=416, input_width=608 , vgg_level=3):
 	x = Dense( 1000 , activation='softmax', name='predictions')(x)
 
 	vgg  = Model(  img_input , x  )
-	#vgg.load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
+	vgg.load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
 
 	levels = [f1 , f2 , f3 , f4 , f5 ]
 
